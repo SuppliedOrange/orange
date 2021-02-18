acf6c06cdf4f8c18d737ab596fea82d2cf237348
SuppliedOrange <70258998+SuppliedOrange@users.noreply.github.com>
2021-02-19 00:40:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports = function orange(string) {
 
-  var isOrange = false;
   let yayitsanorange = "Looks like you've got an orange there. Way to go pal!";
   let dontlooklikeanorange = "Nope, that doesn't look like an orange. Not from around town at least";
   
@@ -13,3 +12,53 @@ module.exports = function orange(string) {
   return dontlooklikeanorange
   };
 };
+module.exports = function includesOrange(string) {
+
+  let yayitsanorange = "I sense an orange among us.";
+  let dontlooklikeanorange = "Just rotten apples here. ROTTEN. APPLES";
+  
+  if (typeof string !== "string") throw new TypeError("You might want to look for an orange in a string");
+  if (string.toLowerCase().includes("orange")) {
+  return yayitsanorange
+  }
+  
+  else {
+  return dontlooklikeanorange
+  };
+};
+
+module.exports = function hasOrange(string) {
+
+  let yayitsanorange = "There is an orange here! An orange!";
+  let dontlooklikeanorange = "Reeks of darn vegetables. No oranges here";
+  
+  if (!Array.isArray(string)) throw new TypeError("i looked far and wide. i couldn't find an array :(");
+  if (string.toLowerCase().includes("orange")) {
+  return yayitsanorange
+  }
+  
+  else {
+  return dontlooklikeanorange
+  };
+};
+
+module.exports = function mixedOrange(string) {
+  
+
+  let yayitsanorange = "ORANGE D E T E C T E D";
+  let dontlooklikeanorange = "Our Arch Wizard tells us there is no orange here. Thanks Megumin.";
+ 
+  
+  if (typeof string !== "string") throw new TypeError("Well this is weird but are you sure your value is a string?");
+  if (string.length > 6) { return dontlooklikeanorange }
+  
+  var orangesort =  ["a", "e", "g", "n", "o", "r"]
+  var stringsort = string.split("").sort()
+
+  
+for (var i = 0; i < stringsort.length; ++i) {
+    if (orangesort[i] !== stringsort[i]) return dontlooklikeanorange;
+  }
+  return yayitsanorange;
+};
+
